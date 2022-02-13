00b0cc2326eb303fdca175754198f7201a0d1ab2
bbahna <moondomi_@naver.com>
2022-02-13 22:33:36 +0900

    styled-components 글로벌 스타일 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <GlobalStyle />
+      <div>Hellow</div>
+    </>
   );
 }
 
