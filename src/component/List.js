2c428f72cd6ac2b0d86c2dd729ff366d294eb9a1
bbahna <moondomi_@naver.com>
2022-02-25 21:29:49 +0900

    state 조회, map() 렌더링 설정

--- src/component/List.js
+++ src/component/List.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 import Item from './Item';
+import { useTodoState } from '../Context';
 
 const TodoList = () => {
+  const todos = useTodoState();
+
   return (
     <List>
-      <Item text="styled-components를 통한 컴포넌트 스타일링" done={true} />
-      <Item text="Context API를 사용한 전역 상태관리" done={true} />
-      <Item text="기능 구현하기(배열 상태 다루기)" done={false} />
+      {todos.map(o => (
+        <Item
+          key={o.id}
+          id={o.id}
+          text={o.text}
+          done={o.done}
+        />
+      ))}
     </List>
   );
 }
