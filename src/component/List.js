ad7e3e21f5e271bab5d213c91812abd70de4d115
bbahna <moondomi_@naver.com>
2022-02-25 21:10:02 +0900

    Context.js와 통일

--- src/component/List.js
+++ src/component/List.js
@@ -6,7 +6,7 @@ const TodoList = () => {
   return (
     <List>
       <Item text="styled-components를 통한 컴포넌트 스타일링" done={true} />
-      <Item text="Context API를 사용한 전역 상태관리" done={false} />
+      <Item text="Context API를 사용한 전역 상태관리" done={true} />
       <Item text="기능 구현하기(배열 상태 다루기)" done={false} />
     </List>
   );
